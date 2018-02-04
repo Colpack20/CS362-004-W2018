95b83fb2fc0c120b2d8d9f57e02c0d898f978013
Colpack20 <Colpack20@gmail.com>
2018-02-04 01:45:40 -0500

    Update cardtest1.c

--- projects/reedkyl/dominion/cardtest1.c
+++ projects/reedkyl/dominion/cardtest1.c
@@ -41,20 +41,9 @@ int main() {
         golds[i] = gold;
     }
 	bonus = 0;
-    printf ("TESTING isGameOver():\n");
-	
-	
-	//for (p = 0; p < numPlayer; p++)
-    //{
-      //  for (handCount = 1; handCount <= maxHandCount; handCount++)
-        //{
-            //for (bonus = 0; bonus <= maxBonus; bonus++)
-            //{
+    printf ("TESTING Smithy():\n");
 				numPlayer = 2;
-				//handCount = 5;
-#if (NOISY_TEST == 1)
-                //printf("Test player %d with %d treasure card(s).\n", p, handCount);
-#endif*/
+
 	printf("test 1 add smithy to index 0 (hand position 1) and use card effect of smithy\n");
                 memset(&G, 23, sizeof(struct gameState));   // clear the game state
                 r = initializeGame(numPlayer, k, seed, &G); // initialize a new game
@@ -67,7 +56,7 @@ int main() {
 				//memcpy(G.hand[1][0], smithy, sizeof(int) * handCount); // set all the cards to copper
                 //int a = isGameOver(&G);
 				//printf("card is %d\n", G.hand[1][0]);
-				printf("player 2 handcount is %d\n", G.handCount[1]);
+				//printf("player 2 handcount is %d\n", G.handCount[1]);
 				//printf("a is %d\n", isGameOver(&G));
 				//printf("whose turn %d\n", G.whoseTurn = 1);
 				G.whoseTurn = 1;
@@ -78,28 +67,28 @@ int main() {
 				printf("deck count is %d\n", G.deckCount[1]);//should be 11
 				int firstDeckcount = G.deckCount[1];
 				for (h = 0; h < 27; h++){
-					printf("card %d is %d\n", h, fullDeckCount(1, h, &G));//should be 11 total
+					//printf("card %d is %d\n", h, fullDeckCount(1, h, &G));//should be 11 total
 					total += fullDeckCount(1, h, &G);
 				}
-				printf("total is %d\n", total);
+				//printf("total is %d\n", total);
 				
-				printf("cardeffect %d \n", cardEffect(smithy, -1, -1, -1, &G, 0, bonus));//play smithy from index 0 of hand
+				int x = cardEffect(smithy, -1, -1, -1, &G, 0, bonus);//play smithy from index 0 of hand
 				//playcard(smithy, -1, -1, -1, &G, 0);
-				printf("new deck count is %d\n", G.deckCount[1]); //is this only counting the cards in the deck or discard pile and not in the hand?				
+				//printf("new deck count is %d\n", G.deckCount[1]); //is this only counting the cards in the deck or discard pile and not in the hand?				
 				int newtotal = 0;
 				for (h = 0; h < 27; h++){
-					printf("card %d is %d\n", h, fullDeckCount(1, h, &G));
+					//printf("card %d is %d\n", h, fullDeckCount(1, h, &G));
 					newtotal += fullDeckCount(1, h, &G);
 				}
-				printf("new total is %d\n", newtotal);//It looks like discarding a card actually ended up trashing it instead of just discarding it.
+				//printf("new total is %d\n", newtotal);//It looks like discarding a card actually ended up trashing it instead of just discarding it.
 				//card, choice1, choice2, choice3, state, handPos, &coin_bonus
 				
-				printf("card is %d\n", G.hand[1][0]);
-				printf("card is %d\n", G.hand[1][1]);
-				printf("card is %d\n", G.hand[1][2]);
-				printf("card is %d\n", G.hand[1][3]);
-				printf("card is %d\n", G.hand[1][4]);
-				printf("handcount is %d\n", G.handCount[1]);
+				//printf("card is %d\n", G.hand[1][0]);
+				//printf("card is %d\n", G.hand[1][1]);
+				//printf("card is %d\n", G.hand[1][2]);
+				//printf("card is %d\n", G.hand[1][3]);
+				//printf("card is %d\n", G.hand[1][4]);
+				//printf("handcount is %d\n", G.handCount[1]);
 				if(G.handCount[1] == 3)
 					printf("test passed, there are three cards in Player 2's hand\n");
 				else
@@ -123,7 +112,7 @@ printf("test 2 add smithy to index 2 (hand position 3) and use card effect of sm
 				G.handCount[1]++;
 				//memcpy(G.hand[1][0], smithy, sizeof(int) * handCount); // set all the cards to copper
                 //int a = isGameOver(&G);
-				printf("handcount initial %d\n", G.handCount[1]);
+				//printf("handcount initial %d\n", G.handCount[1]);
 				
 				//printf("a is %d\n", isGameOver(&G));
 				//printf("whose turn %d\n", G.whoseTurn = 1);
@@ -132,31 +121,31 @@ printf("test 2 add smithy to index 2 (hand position 3) and use card effect of sm
 				
 				//G.deck[1][10] = smithy;
 				//G.deckCount[1]++;
-				printf("deck count is %d\n", G.deckCount[1]);//should be 11
+				//printf("deck count is %d\n", G.deckCount[1]);//should be 11
 				firstDeckcount = G.deckCount[1];
 				for (h = 0; h < 27; h++){
-					printf("card %d is %d\n", h, fullDeckCount(1, h, &G));//should be 11 total
+					//printf("card %d is %d\n", h, fullDeckCount(1, h, &G));//should be 11 total
 					total += fullDeckCount(1, h, &G);
 				}
-				printf("total is %d\n", total);
+				//printf("total is %d\n", total);
 				
-				printf("cardeffect %d \n", cardEffect(smithy, -1, -1, -1, &G, 2, bonus));//play smithy from index 0 of hand
+				//printf("cardeffect %d \n", cardEffect(smithy, -1, -1, -1, &G, 2, bonus));//play smithy from index 0 of hand
 				//playcard(smithy, -1, -1, -1, &G, 0);
-				printf("new deck count is %d\n", G.deckCount[1]); //is this only counting the cards in the deck or discard pile and not in the hand?				
+				//printf("new deck count is %d\n", G.deckCount[1]); //is this only counting the cards in the deck or discard pile and not in the hand?				
 				newtotal = 0;
 				for (h = 0; h < 27; h++){
-					printf("card %d is %d\n", h, fullDeckCount(1, h, &G));
+					//printf("card %d is %d\n", h, fullDeckCount(1, h, &G));
 					newtotal += fullDeckCount(1, h, &G);
 				}
-				printf("new total is %d\n", newtotal);//It looks like discarding a card actually ended up trashing it instead of just discarding it.
+				//printf("new total is %d\n", newtotal);//It looks like discarding a card actually ended up trashing it instead of just discarding it.
 				//card, choice1, choice2, choice3, state, handPos, &coin_bonus
 				
-				printf("card is %d\n", G.hand[1][0]);
-				printf("card is %d\n", G.hand[1][1]);
-				printf("card is %d\n", G.hand[1][2]);
-				printf("card is %d\n", G.hand[1][3]);
-				printf("card is %d\n", G.hand[1][4]);
-				printf("handcount is %d\n", G.handCount[1]);
+				//printf("card is %d\n", G.hand[1][0]);
+				//printf("card is %d\n", G.hand[1][1]);
+				//printf("card is %d\n", G.hand[1][2]);
+				//printf("card is %d\n", G.hand[1][3]);
+				//printf("card is %d\n", G.hand[1][4]);
+				//printf("handcount is %d\n", G.handCount[1]);
 				if(G.handCount[1] == 3)
 					printf("test passed, there are three cards in Player 2's hand\n");
 				else
